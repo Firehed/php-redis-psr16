89c49d0e75b0c51febfb0fa32a69052b111a5fc7
Eric Stern <eric@ericstern.com>
2021-06-04 13:07:30 -0700

    Test back and forth encoding of all data

--- tests/integration/IntegrationTest.php
+++ tests/integration/IntegrationTest.php
@@ -59,6 +59,66 @@ class IntegrationTest extends \PHPUnit\Framework\TestCase
         self::assertEqualsCanonicalizing($data, $afterSetting);
     }
 
+    public function testObjectSerialization(): void
+    {
+        $object = new SampleObject(3, 'three', 3.14, true);
+        $cache = new RedisPsr16($this->redis);
+        $cache->set(__METHOD__, $object);
+        $response = $cache->get(__METHOD__);
+        self::assertEquals($object, $response, 'Object changed (loose equality)');
+    }
+
+    /**
+     * @dataProvider encodingValues
+     */
+    public function testValueSerialization(mixed $value): void
+    {
+        $cache = new RedisPsr16($this->redis);
+        $cache->set(__METHOD__, $value);
+        $response = $cache->get(__METHOD__);
+        self::assertSame($value, $response, 'Value changed when returned from cache');
+    }
+
+    /**
+     * @see https://www.php-fig.org/psr/psr-16/ Section 1.4
+     *
+     * > If it is not possible to return the exact saved value for any reason,
+     * > implementing libraries MUST respond with a cache miss rather than
+     * > corrupted data.
+     */
+    public function testFalseHandling(): void
+    {
+        $value = false;
+        $cache = new RedisPsr16($this->redis);
+        $cache->set(__METHOD__, $value);
+        $response = $cache->get(__METHOD__, 'default');
+        self::assertSame(
+            'default',
+            $response,
+            '`false` cannot be correctly retreived and must be treated as a cache miss',
+        );
+    }
+
+    /**
+     * @return array{mixed}[]
+     */
+    public function encodingValues(): array
+    {
+        return array_map(fn ($v) => [$v], [
+            'string',
+            0,
+            PHP_INT_MIN,
+            PHP_INT_MAX,
+            -3.14,
+            3.14,
+            true,
+            null,
+            [1, 2, 3],
+            ['a' => 1, 'b' => 2, 'c' => 3],
+            ['a' => ['b' => ['c' => [1, 2, 3]]]],
+        ]);
+    }
+
     public function testReconnect(): void
     {
         self::markTestSkipped('Future functionality');
