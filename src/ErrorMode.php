1cd8e601488ab949b5d558bf2b40e52b688bde5f
Eric Stern <eric@ericstern.com>
2021-10-20 16:59:28 -0700

    Use class_alias hack to avoid symbol breakage under 8.0

--- src/ErrorMode.php
+++ src/ErrorMode.php
@@ -5,15 +5,7 @@ declare(strict_types=1);
 namespace Firehed\Cache;
 
 if (version_compare(PHP_VERSION, '8.1.0', '>=')) {
-    enum ErrorMode
-    {
-        case EXCEPTION;
-        case FAIL;
-    }
+    class_alias(ErrorModeGTE81::class, ErrorMode::class);
 } else {
-    interface ErrorMode
-    {
-        public const EXCEPTION = 0;
-        public const FAIL = 1;
-    }
+    class_alias(ErrorModeLT81::class, ErrorMode::class);
 }
---
+++ src/ErrorModeGTE81.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Firehed\Cache;
+
+enum ErrorModeGTE81
+{
+    case EXCEPTION;
+    case FAIL;
+}
