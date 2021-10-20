3b32fafc59ac55a2d25ece65b352d683fc7ed9ae
Eric Stern <eric@ericstern.com>
2021-10-20 16:56:14 -0700

    Use native enums in PHP 8.1

--- src/ErrorMode.php
+++ src/ErrorMode.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace Firehed\Cache;
 
-interface ErrorMode
-{
-    public const EXCEPTION = 0;
-    public const FAIL = 1;
+if (version_compare(PHP_VERSION, '8.1.0', '>=')) {
+    enum ErrorMode
+    {
+        case EXCEPTION;
+        case FAIL;
+    }
+} else {
+    interface ErrorMode
+    {
+        public const EXCEPTION = 0;
+        public const FAIL = 1;
+    }
 }
